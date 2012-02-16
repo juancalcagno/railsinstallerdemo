3e558e60462cdb55d58f477a241910e909a04df6
Juan L. Calcagno <juan.calcagno@gmail.com>
2012-02-16 18:52:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainstallerdemo::Application.config.secret_token = '6460a1117b47fbfbfa12057f165d9c7ee84db9133164fc501fdb0545329f79ddfc727eae4d961a9519c4bb5695ff73d21288ea7e57c550297450c1d3f6cf6d85'
